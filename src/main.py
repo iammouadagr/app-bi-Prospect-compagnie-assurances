f185b3a2fa6ea81857cb32252b065a3095e555ec
Mouad Aguirar <mouad.aguirar@alumni.univ-avignon.fr>
2023-04-02 19:20:31 +0200

    feat: add main script

--- src/main.py
+++ src/main.py
@@ -1,37 +1,19 @@
-from clean_dataset import clean_supervised,clean_unsupervised
+from clean_dataset import clean_supervised, clean_unsupervised
+import subprocess
 
-menu_options = {
-    1: 'Analyse descriptive',
-    2: 'Option 2',
-    3: 'Option 3',
-    4: 'Exit',
-}
-
-def print_menu():
-    for key in menu_options.keys():
-        print (key, '--', menu_options[key] )
-
-
-def data_exploration():
-    clean_dataset_for_supervised_classification = clean_supervised()
-    clean_dataset_for_unsupervised_classification = clean_unsupervised()
-
-
-
-
+def clean_dataset():
+    print("####  Préparation des données  ####")
+    clean_supervised()
+    clean_unsupervised()
 
 
 if __name__ == "__main__" :
-    while (True):
-        print_menu()
-        option = ''
-        try:
-            option = int(input('Enter your choice: '))
-        except:
-            print('Wrong input. Please enter a number ...')
-        # Check what choice was entered and act accordingly
-        if option == 1:
-            data_exploration()
-        elif option == 2:
-            print('Thanks message before exiting')
-            exit()
+    print("------------------------------------")
+    clean_dataset()
+    print("------------------------------------")
+    print("####  Classification non supervisée  ####")
+    subprocess.run(["python", "unsupervise.py"])
+    print("------------------------------------")
+    print("####  Classification supervisée  ####")
+    subprocess.run(["python", "supervised.py"])
+
